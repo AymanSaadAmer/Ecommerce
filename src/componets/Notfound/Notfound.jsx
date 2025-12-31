6bdbbe1ea2ea1da21825c77221ad18553b018f8a
AymanSaadAmer <aymansaadebrahimamer@gmail.com>
2026-01-01 00:19:10 +0200

    Update Notfound page Secound once

--- src/componets/Notfound/Notfound.jsx
+++ src/componets/Notfound/Notfound.jsx
@@ -6,10 +6,10 @@ export default function Notfound() {
   return (
     <>
       <div className="text-center w-[80%] my-1.5 m-auto">
-        <h1>
+        <h1 className='text-4xl font-bold !text-green-600 !my-8'>
             Wellcome Notfound
         </h1>
-        <h3>please go to login page or register page to continue</h3>
+        <h3 className='!mb-12'>please go to login page or register page to continue</h3>
       </div>
     </>
   )
